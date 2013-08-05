e641b59213392479efe4bc21ff2cddfd2b488cbb
PewDiePie <sidthekid402@gmail.com>
2013-08-05 14:29:04 -0700

    Update Fading.ino

--- Motors-and-Lights/Fading.ino
+++ Motors-and-Lights/Fading.ino
@@ -17,3 +17,24 @@ for(int i = 0; i<=255; i++)
     analogWrite(9,i);
     delay(10);
   }
+    
+    
+    
+}
+
+void fadeOut(int x)
+  {
+    for(int i = 255; i >= 0; i--)
+  analogWrite(9,i); //always on
+  delay(x);
+{
+  void fadeIn(int x);
+  for(int i = 0; i<=255; i++)
+  {
+    analogWrite(9,i);
+    delay(x);
+
+  }
+}
+  }
+
